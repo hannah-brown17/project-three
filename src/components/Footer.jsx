0871223f1160b29603e25017621c44034e751b61
hannah-brown17 <hannahdbrown10@gmail.com>
2025-02-03 19:39:11 -0500

    made footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Home</p>
+      <p>Articles</p>
+      <p>Webinars</p>
+      <p>Membership</p>
+      <p>About</p>
+    </div>
+  )
+}
+
+export default Footer
