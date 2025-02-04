9d32b68f6d265d9a6b2e6c1192908f5064f2b30d
hannah-brown17 <hannahdbrown10@gmail.com>
2025-02-03 20:38:29 -0500

    added a subscribe to newsletter button located in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Home</p>
-      <p>Articles</p>
-      <p>Videos</p>
-      <p>Membership</p>
-      <p>About</p>
+      <h5>Subscribe to Our Newsletter</h5>
+      <form action="#" method="POST">
+        <label for="email">Enter your email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+        ></input>
+        <button type="submit">Subscribe</button>
+      </form>
     </div>
   )
 }
